a52bc2422c920907073ecb391e8ee14af5c80fdf
John Thompson <john@springframework.guru>
2025-12-18 17:28:25 -0500

    Fix package declaration in `MySqlTest.java`

--- src/test/java/guru/springframework/spring7restmvc/repositories/MySqlTest.java
+++ src/test/java/guru/springframework/spring7restmvc/repositories/MySqlTest.java
@@ -1,4 +1,4 @@
-package guru.springframework.spring6restmvc.repositories;
+package guru.springframework.spring7restmvc.repositories;
 
 import guru.springframework.spring7restmvc.entities.Beer;
 import org.junit.jupiter.api.Test;
