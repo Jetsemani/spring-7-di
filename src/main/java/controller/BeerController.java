4e43c91808c9017b83c66f4b2218f6f7228749e0
Jetse <jetsemani81@gmail.com>
2026-03-09 16:30:47 +0100

    lombok-logging

--- src/main/java/controller/BeerController.java
+++ src/main/java/controller/BeerController.java
@@ -1,11 +1,24 @@
 package controller;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import model.Beer;
 import org.springframework.stereotype.Controller;
 import services.BeerService;
 
+import java.util.UUID;
+
+@Slf4j
 @AllArgsConstructor
 @Controller
 public class BeerController {
+
     private final BeerService beerService;
+
+    public Beer getBeerById(UUID id) {
+
+        log.debug("Get Beer by Id -- In Controller");
+
+        return beerService.getBeerById(id);
+    }
 }
