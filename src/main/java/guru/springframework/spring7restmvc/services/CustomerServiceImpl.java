73cb9cc84d8dc6b82ab84f52e67e4ea4ce618199
Jetsemani <jetsemani81@gmail.com>
2026-03-10 15:26:11 +0100

    patch-customer

--- src/main/java/guru/springframework/spring7restmvc/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/spring7restmvc/services/CustomerServiceImpl.java
@@ -3,6 +3,7 @@ package guru.springframework.spring7restmvc.services;
 import guru.springframework.spring7restmvc.model.Customer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.time.LocalDateTime;
 import java.util.*;
@@ -82,5 +83,15 @@ public class CustomerServiceImpl implements CustomerService {
     @Override
     public void deleleteById(UUID customerId) {
         customerMap.remove(customerId);
+    }
+
+    @Override
+    public void patchCustomerById(UUID customerId, Customer customer) {
+
+        Customer existing = customerMap.get(customerId);
+
+        if (StringUtils.hasText(customer.getCustomerName())) {
+            existing.setCustomerName(customer.getCustomerName());
+        }
     }
 }
