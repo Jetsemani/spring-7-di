7a6c85e7ea23685100f9d9958fd52ff2ef26ebac
Jetsemani <jetsemani81@gmail.com>
2026-03-12 15:43:25 +0100

    spring-jpa-get-operations

---
+++ src/main/java/guru/springframework/spring7restmvc/services/CustomerServiceJPA.java
@@ -0,0 +1,51 @@
+package guru.springframework.spring7restmvc.services;
+
+import guru.springframework.spring7restmvc.mappers.CustomerMapper;
+import guru.springframework.spring7restmvc.model.CustomerDTO;
+import guru.springframework.spring7restmvc.repositories.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class CustomerServiceJPA implements CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
+
+    @Override
+    public List<CustomerDTO> listCustomers() {
+        return List.of();
+    }
+
+    @Override
+    public Optional<CustomerDTO> getCustomerById(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public CustomerDTO saveNewCustomer(CustomerDTO customer) {
+        return null;
+    }
+
+    @Override
+    public void updateCustomerById(UUID customerId, CustomerDTO customer) {
+
+    }
+
+    @Override
+    public void deleleteById(UUID customerId) {
+
+    }
+
+    @Override
+    public void patchCustomerById(UUID customerId, CustomerDTO customer) {
+
+    }
+}
